1e58bc9ed649b1cfab6db5340b2370213d8ea67d
Edmond Pruteanu <edmond@swiftshift.com>
2016-12-18 00:35:23 +0200

    Added DB connection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,32 @@
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+import helper.Utility;
+import models.User;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+import java.sql.SQLException;
+
 import static spark.Spark.*;
 
 public class Main {
 
     public static void main(String[] args) {
         port(getHerokuAssignedPort());
-        get("/hello", (req, res) -> "Hello schele popi panouri World");
+        get("/hello", (req, res) -> "Hello Schele World");
+
+        Spark.get("/users", new Route() {
+            public Object handle(Request request, Response response) {
+                return  "User: username=test, email=test@test.net";
+            }
+        });
+
+        connectToDB();
     }
 
     static int getHerokuAssignedPort() {
@@ -13,6 +35,64 @@ public class Main {
             return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
         return 4567;
+    }
+
+    private static void connectToDB() {
+        String databaseUrl = Utility.DB_URL;
+
+        try {
+            ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrl);
+            ((JdbcConnectionSource)connectionSource).setUsername(Utility.DB_USER);
+            ((JdbcConnectionSource)connectionSource).setPassword(Utility.DB_PASSWORD);
+
+            Dao<User,String> userDao = DaoManager.createDao(connectionSource, User.class);
+            TableUtils.createTableIfNotExists(connectionSource, User.class);
+
+            post("/users", (request, response) -> {
+                try {
+                    String username = request.queryParams("username");
+                    String email = request.queryParams("email");
+
+                    User user = new User();
+                    user.setUsername(username);
+                    user.setEmail(email);
+
+                    try {
+                        userDao.create(user);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+
+                    response.status(201);
+                    response.type("application/json");
+                    return user;
+                } catch (Exception e) {
+                    return "";
+                }
+            });
+
+            Spark.get("/users/:id", new Route() {
+                @Override
+                public Object handle(Request request, Response response) {
+                    try {
+                        User user = userDao.queryForId(request.params(":id"));
+                        if (user != null) {
+                            return "Username: " + user.getUsername(); // or JSON? :-)
+                        } else {
+                            response.status(404); // 404 Not found
+                            return "User not found";
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    return response;
+                }
+            });
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
---
+++ src/main/java/helper/Utility.java
@@ -0,0 +1,17 @@
+package helper;
+
+/**
+ * Created by edmond on 17/12/2016.
+ */
+public class Utility {
+
+    /*public final static String DB_URL = "jdbc:mysql://localhost/schelepopi";
+    public final static String DB_USER = "root";
+    public final static String DB_PASSWORD = "123456";*/
+
+    public final static String DB_URL = "jdbc:mysql://us-cdbr-iron-east-04.cleardb.net/heroku_4d4cca9bc382e53";
+    public final static String DB_USER = "b700b41e834b8a";
+    public final static String DB_PASSWORD = "1519ea70";
+
+}
+
